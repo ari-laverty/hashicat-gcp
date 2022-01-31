a97116c05c1ae84c7e538324c9bc11691ccafdde
ari-laverty <91864504+ari-laverty@users.noreply.github.com>
2022-01-31 16:23:49 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,10 +6,10 @@ module "vpc" {
     routing_mode = "GLOBAL"
     subnets = [
         {
-            subnet_name = "gaurav-subnet"
-            subnet_ip = "10.100.10.0/24"
+            subnet_name   = "gaurav-subnet"
+            subnet_ip     = "10.100.10.0/24"
             subnet_region = var.region
-        },
+        }
     ]
     
 }
