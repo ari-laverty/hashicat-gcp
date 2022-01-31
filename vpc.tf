a21d9bae13e0e3882cf720e4553c205c94235ec4
ari-laverty <91864504+ari-laverty@users.noreply.github.com>
2022-01-31 16:10:01 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
     source = "app.terraform.io/agbg-australia/network/google"
     version = "3.4.0"
     project_id = var.project
-    network_name = "example-vpc"
+    network_name = "gaurav-network"
     routing_mode = "GLOBAL"
     subnets = [
         {
